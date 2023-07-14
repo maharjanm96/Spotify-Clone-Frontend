1be2eee4d1f43d5d2caacb7ad890cfc2b1d14589
Manish Maharjan <maharjanm96@gmail.com>
2023-07-14 20:36:08 +0545

    react-toastify Added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './global.css';
 import App from './App';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
